3ea534fa8768354607c501daca05779d7c905f64
fmt <1105896230@qq.com>
2016-09-03 14:24:42 +0800

    test

--- redemo3.py
+++
@@ -1,8 +0,0 @@
-#-*-coding:utf-8-*
-import requests
-import re
-
-html =requests.get('https://segmentfault.com/q/1010000002740016')
-str=html.text
-tag=re.findall('class="btn btn-primary btn-sm tag-mgr__btn">(.*?)</a>',str,re.S)
-print(tag)
--- regx/class1.py
+++
@@ -1,4 +0,0 @@
-
-import regx
-
-print ("hello world")
